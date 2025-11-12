33039a7b2f8f3b195871b7997a5ed259399335f6
FaridVR <martinesfranco510@gmail.com>
2025-11-11 22:24:29 -0600

    Add files via upload

---
+++ hol.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;{           
+int main() 
+int x ;
+cout << "ingresa el  numero que deseas imprimir: ";
+cin >> x ;
+for (int i = 1; i <= x; i++) {
+
+
+
+
+}
+}
